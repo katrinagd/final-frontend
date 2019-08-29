9a7c68d9398b8a24a4d03cbcf1e0a08b02e27192
Katrinagd <katrinaguyduy@gmail.com>
2019-08-29 06:01:45 -0700

    form re-work

--- src/components/auth/Form.js
+++ src/components/auth/Form.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import { withRouter } from 'react-router'
-// import * as EmailValidator from 'email-validator'
 
 class Form extends React.Component {
   constructor (props) {
     super(props)
-    // this.errorFocus = React.createRef();
 
     this.state = {
       email: '',
@@ -16,7 +14,6 @@ class Form extends React.Component {
     }
 
     this.handleChange = this.handleChange.bind(this)
-    // this.handleError = this.handleError.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
@@ -27,52 +24,6 @@ class Form extends React.Component {
       errors: []
     })
   }
-
-  // handleError({ target: { name, value } }) {
-  //   if (name === 'first_name') {
-  //     if (!value) {
-  //       this.setState({ first_nameError: 'Error: First name is required' })
-  //     } else {
-  //       this.setState({ first_nameError: '' })
-  //       delete this.state.first_nameError;
-  //     }
-  //   }
-  //   if (name === 'last_name') {
-  //     if (!value) {
-  //       this.setState({ last_nameError: 'Error: Last name is required' })
-  //     } else {
-  //       this.setState({ last_nameError: '' })
-  //       delete this.state.last_nameError;
-  //     }
-  //   }
-  //   if (name === 'email') {
-  //     const validEmail = EmailValidator.validate(this.state.email)
-  //     if (!value) {
-  //       this.setState({ emailError: 'Error: Email address is required' })
-  //     } else if (!validEmail) {
-  //       this.setState({ emailError: 'Error: You must enter a valid email address' })
-  //     } else {
-  //       this.setState({ emailError: '' })
-  //       delete this.state.emailError;
-  //     }
-  //   }
-  //   if (name === 'password') {
-  //     const validPassword = this.state.password.length >= 8
-  //     if (!value) {
-  //       this.setState({ passwordError: 'Error: Password is required' })
-  //     } else if (!validPassword) {
-  //       this.setState({ passwordError: 'Error: Your password must be at least 8 characters' })
-  //     } else {
-  //       this.setState({ passwordError: '' })
-  //       delete this.state.passwordError;
-  //     }
-  //   }
-  //   if (!this.state.first_nameError && !this.state.last_nameError && !this.state.passwordError && !this.state.emailError) {
-  //     this.setState({ formError: '' })
-  //     delete this.state.formError;
-  //   }
-        
-  // }
 
 
   handleSubmit(e) {
@@ -113,11 +64,9 @@ class Form extends React.Component {
                 className='form-control validate_input'
                 id='email'
                 onChange={this.handleChange}
-                // onBlur={this.handleError}
                 name='email'
                 type='text'
                 value={this.state.email}
-                // required
           />
         </div>
         <div className='form-group col-md-6'>
@@ -129,7 +78,6 @@ class Form extends React.Component {
             name='password'
             type='password'
             value={this.state.password}
-            // required
           />
         </div>
         </div>
@@ -141,11 +89,9 @@ class Form extends React.Component {
               className='form-control'
               id='firstName'
               onChange={this.handleChange}
-              // onBlur={this.handleError}
               name='first_name'
               type='text'
               value={this.state.firstName}
-              // required
             />
           </div>
           <div className='form-group col-md-6'>
@@ -154,22 +100,22 @@ class Form extends React.Component {
               className='form-control'
               id='lastName'
               onChange={this.handleChange}
-              // onBlur={this.handleError}
               name='last_name'
               type='text'
               value={this.state.lastName}
-              // required
             />
             
           </div> 
           </div>
         }
         <button type='submit' className='btn btn-info'>Submit</button>
+        <div> 
         {this.state.errors.length > 0 &&
           <div className='alert alert-danger'>
             {this.state.errors.map(txt => <p>{txt}</p>)}
           </div>
         }
+        </div>
         </form>
     )
   }
